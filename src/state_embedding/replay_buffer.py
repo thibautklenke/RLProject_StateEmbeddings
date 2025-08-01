4f8e8733b01795bae01600306f0579bca8c98428
ostylk <ostylk@googlemail.com>
2025-08-01 19:51:18 +0200

    numpify replay buffer (and make it work lol)

--- src/state_embedding/replay_buffer.py
+++ src/state_embedding/replay_buffer.py
@@ -8,7 +8,13 @@ from typing import Any
 
 
 class ContextualizedReplayBuffer(ReplayBuffer):
-    _context: list[list[np.ndarray]]
+    # [window_size, n_envs, *obs_shape]
+    _context_tracker: np.ndarray
+    # [n_envs]
+    _context_index: np.ndarray
+
+    # [buffer_size, window_size, n_envs, *obs_shape]
+    contexts: np.ndarray
 
     def __init__(
         self,
@@ -33,16 +39,16 @@ class ContextualizedReplayBuffer(ReplayBuffer):
 
         self._window_size: int = window_size
 
-        self._context = list()
+        self._context_tracker = np.zeros(
+            (self._window_size, self.n_envs, *self.obs_shape),
+            dtype=observation_space.dtype,
+        )
+        self._context_index = np.zeros((self.n_envs,), dtype=np.int64)
 
-    def _resize_context(self, size: int):
-        diff = size - len(self._context)
-        if diff > 0:
-            for _ in range(diff):
-                self._context.append(list())
-        elif diff < 0:
-            for _ in range(-diff):
-                self._context.pop(-1)
+        self.contexts = np.zeros(
+            (self.buffer_size, self._window_size, self.n_envs, *self.obs_shape),
+            dtype=observation_space.dtype,
+        )
 
     def add(
         self,
@@ -53,18 +59,35 @@ class ContextualizedReplayBuffer(ReplayBuffer):
         done: np.ndarray,
         infos: list[dict[str, Any]],
     ):
-        self._resize_context(obs.shape[0])
+        # Reshape needed when using multiple envs with discrete observations
+        # as numpy cannot broadcast (n_discrete,) to (n_discrete, 1)
+        if isinstance(self.observation_space, spaces.Discrete):
+            obs = obs.reshape((self.n_envs, *self.obs_shape))
+            next_obs = next_obs.reshape((self.n_envs, *self.obs_shape))
 
-        n_envs = len(self._context)
-        for i in range(n_envs):
-            self._context[i].append(obs[i])
-            if len(self._context[i]) > self._window_size:
-                self._context[i].pop(0)
+        # Write current observations to context tracker
+        for env in range(self.n_envs):
+            idx = self._context_index[env]
+            self._context_tracker[idx, env, :] = obs[env, :]
 
-            infos[i]["context"] = self._context[i][:]  # Copy into infos
+        # Put current context into the replay buffer
+        self.contexts[self.pos, :] = self._context_tracker
 
+        # Increment window index for each environment
+        for env in range(self.n_envs):
+            # If the index is at the end of the window, shift all contexts
+            if self._context_index[env] == self._window_size - 1:
+                for idx in range(self._window_size - 1):
+                    self._context_tracker[idx, env, :] = self._context_tracker[
+                        idx + 1, env, :
+                    ]
+            else:
+                self._context_index[env] += 1
+
+        # If environment is done, reset the context tracker for that environment
         for idx, terminate in enumerate(done):
             if terminate:
-                self._context[idx] = list()
+                self._context_tracker[:, idx, :] = 0
+                self._context_index[idx] = 0
 
         super().add(obs, next_obs, action, reward, done, infos)
