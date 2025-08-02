139faed2d3a52909b4325050dd2bbdd4e6da17c7
Thibaut Klenke <thibaut.klenke@stud.uni-hannover.de>
2025-08-02 13:11:21 +0200

    Transformer?

--- src/state_embedding/embed.py
+++ src/state_embedding/embed.py
@@ -1,12 +1,10 @@
 from stable_baselines3.dqn import DQN
-from stable_baselines3.dqn.policies import QNetwork, DQNPolicy
+from stable_baselines3.dqn.policies import DQNPolicy
 from torch.nn import functional as F
-from stable_baselines3.common.type_aliases import PyTorchObs
 import torch as th
 from torch import nn
 import numpy as np
 from gymnasium import spaces
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from typing import Optional, Union, Any
 from stable_baselines3.common.type_aliases import GymEnv, Schedule
 from state_embedding.replay_buffer import ContextualizedReplayBuffer
@@ -39,7 +37,7 @@ class DQNWithEmbedLoss(DQN):
         seed: Optional[int] = None,
         device: Union[th.device, str] = "auto",
         _init_setup_model: bool = True,
-        window_size: int = 10,  # Embedding context
+        window_size: int = 5,  # Embedding context
     ):
         if replay_buffer_kwargs is None:
             replay_buffer_kwargs = {"window_size": window_size}
@@ -87,9 +85,13 @@ class DQNWithEmbedLoss(DQN):
         losses = []
         for _ in range(gradient_steps):
             # Sample replay buffer
-            replay_data = self.replay_buffer.sample(
+            replay_data, context = self.replay_buffer.sample_with_context(
                 batch_size, env=self._vec_normalize_env
             )  # type: ignore[union-attr]
+
+            # [batch_size, window_size, *obs]
+            context = th.flatten(context, start_dim=2)
+            # [batch_size, window_size, num_features]
 
             with th.no_grad():
                 # Compute the next Q-values using the target network
@@ -132,41 +134,36 @@ class DQNWithEmbedLoss(DQN):
         self.logger.record("train/loss", np.mean(losses))
 
 
-class StateEmbedNetwork(QNetwork):
-    # TODO: introduce flag whether network uses "vanilla" or "custom" behaviour
-    #   Needed because we want to introduce a second loss, but
-    #   also to be able to insert this special state module into normal DQN
-
+class StateEmbedNetwork(nn.Module):
     def __init__(
         self,
         observation_space: spaces.Space,
-        action_space: spaces.Discrete,
-        features_extractor: BaseFeaturesExtractor,
-        features_dim: int,
-        net_arch: Optional[list[int]] = None,
-        activation_fn: type[nn.Module] = nn.ReLU,
-        normalize_images: bool = True,
-        k: int = 10,
+        n_head=8,
+        n_layers_encoder=6,
+        n_layers_decoder=6,
+        window_size: int = 10,
+        # embedding_size =
     ):
-        super().__init__(
-            observation_space,
-            action_space,
-            features_extractor,
-            features_dim,
-            net_arch,
-            activation_fn,
-            normalize_images,
+        super().__init__()
+
+        self._window_size = window_size  # Window size for contextual embedding
+        num_features = np.prod(observation_space.shape)
+
+        # self._linear = nn.Linear(num_features, num_features)
+
+        self._encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(num_features, n_head, batch_first=True),
+            n_layers_encoder,
         )
 
-        self._custom = False  # Whether to use vanilla or custom behavior
-        self._k = k  # Window size for contextual embedding
+        self._decoder = nn.TransformerDecoderLayer(
+            nn.TransformerDecoderLayer(num_features, n_head, batch_first=True),
+            n_layers_decoder,
+        )
 
     # @override
-    def forward(self, obs: PyTorchObs) -> th.Tensor:
-        # TODO: maybe use own feature extractor
-        z = self.extract_features(obs, self.features_extractor)
+    def forward(self, context: th.Tensor) -> th.Tensor:
+        encoded = self._encoder(context)
+        decoded = self._decoder(encoded)
 
-        # TODO: put z through decoder (e.g. "unextract_features")
-        #   to compute loss
-
-        return self.q_net(z)
+        return decoded
