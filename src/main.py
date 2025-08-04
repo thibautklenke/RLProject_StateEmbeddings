dd98d7e1d7addda83ab328431cc947dd3739b308
Niklas Baack <niklasbaack@gmail.com>
2025-08-04 19:28:03 +0200

    [+] loop through seeds & nicer graphs

--- src/main.py
+++ src/main.py
@@ -1,30 +1,43 @@
 import gymnasium as gym
+import numpy as np
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.callbacks import ProgressBarCallback, EvalCallback
 
 def call_eval_loop() -> None:
 
+    envs = ["CartPole-v1"]
+    seeds = np.random.randint(low=0, high=10000, size=100)
+    models = ["DQN"]
+
     # Parameter
-    env_name = "CartPole-v1"
-    SEED = 0
+    for i in range(100):
+        ENV_NAME = np.random.choice(envs)
+        SEED = int(np.random.choice(seeds))
+        MODEL_NAME = np.random.choice(models)
+
+        eval_loop(MODEL_NAME, ENV_NAME, SEED)
+
+def eval_loop(model_name, env_name, seed)->None:
 
     # create the main variables
     env = gym.make(env_name)
-    env.reset(seed=SEED)
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/MlpPolicy/")
+    env.reset(seed=seed)
+
+    if model_name == "PPO":
+        model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/MlpPolicy/")
+    elif model_name == "DQN":
+        model = DQN("MlpPolicy", env, verbose=1, tensorboard_log="./logs/MlpPolicy/", learning_rate=1e-3)
 
     # Separate evaluation env
     eval_env = gym.make(env_name)
     # Use deterministic actions for evaluation
-    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
-                                 log_path="./logs/", eval_freq=500,
+    eval_callback = EvalCallback(eval_env,  eval_freq=500,
                                  deterministic=True, render=False)
 
     # run the model with the callbacks
-    model.learn(total_timesteps=10000, callback=[ProgressBarCallback(), eval_callback])
-
-def graphs() -> None:
-    print("Done")
+    model.learn(total_timesteps=10000,
+                callback=[ProgressBarCallback(), eval_callback],
+                tb_log_name=f"{model_name}-{env_name}-{seed}",)
 
 
 if __name__ == "__main__":
