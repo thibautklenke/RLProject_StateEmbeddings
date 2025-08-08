83f1bb46de9fa37e1d6b207af721586d871804bb
Thibaut Klenke <thibaut.klenke@stud.uni-hannover.de>
2025-08-08 13:24:55 +0200

    update main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import breakout_experiment as be
 import cartpole_experiment as ce
 import seaquest_experiment as se
 import seaquestmarkov_experiment as sem
+import minigrid_memory_experiment as mme
+import minigrid_unlock_experiment as mue
 import random
 import gymnasium as gym
 import numpy as np
@@ -19,6 +21,16 @@ def main() -> None:
     seeds = range(1)
     register_envs()  # Register minatar namespace for gymnasium
 
+    # Pretrain only once
+    mme.pretrain(0)
+    mue.pretrain(0)
+    # be.pretrain(0)
+    # ce.pretrain(0)
+    # se.pretrain(0)
+    # sem.pretrain(0)
+
+    return
+
     for SEED in seeds:
         print(SEED)
         set_random_seed(SEED)
@@ -26,8 +38,6 @@ def main() -> None:
         np.random.seed(SEED)
         random.seed(SEED)
 
-        ce.pretrain(SEED)
-        ce.train(SEED)
 
 
 if __name__ == '__main__':
